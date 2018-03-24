58d5c73c5d92d3c6882e0c4400883e3b2ddbe989
Madhu Mankal <mankalmadhu@gmail.com>
2018-03-24 18:43:52 +0100

    Use first excersice in main

--- main/src/main/cpp/main.cpp
+++ main/src/main/cpp/main.cpp
@@ -1,7 +1,12 @@
 #include "hello.h"
+#include <FirstExcersice.hpp>
+#include <iostream>
 
 int main () {
     Greeter greeter;
     greeter.hello();
+
+    std::cout << FirstExcersice().doIt()<<std::endl;
+
     return 0;
 }
